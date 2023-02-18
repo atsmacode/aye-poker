db0d6adb1b107f25ca5a403fb3045f859a5ffca3
Mark Ramage <mramage@hotmail.co.uk>
2023-02-18 09:46:04 +0000

    Add basic Webpack set-up and template styles

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -23,10 +23,4 @@ class LoginController extends AbstractController
             'error'         => $error,
         ]);
     }
-    // public function index(): Response
-    // {
-    //     return $this->render('login/index.html.twig', [
-    //         'controller_name' => 'LoginController',
-    //     ]);
-    // }
 }
--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class PlayController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
+    #[Route('/play', name: 'app_play')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('play/index.html.twig', [
+            'controller_name' => 'PlayController',
         ]);
     }
 }
