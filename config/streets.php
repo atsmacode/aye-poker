7c303d3a3f3f3047d9fba2f92ceffa2cbe8aa847
Mark Ramage <mramage@hotmail.co.uk>
2023-02-27 06:51:04 +0000

    Add dev commands, street config and read-me

---
+++ config/streets.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'name' => 'Pre-flop'
+    ],
+    [
+        'name' => 'Flop',
+    ],
+    [
+        'name' => 'Turn',
+    ],
+    [
+        'name' => 'River',
+    ],
+];
