568305a997d4fd3576d87f0fe8e5bf5a8f632b3e
Mark <mark@Marks-MacBook-Pro.local>
2025-04-06 09:25:37 +0100

    Edit comment

--- modules/poker-game/app/GamePlay/Game/Game.php
+++ modules/poker-game/app/GamePlay/Game/Game.php
@@ -4,6 +4,9 @@ namespace Atsmacode\PokerGame\GamePlay\Game;
 
 /**
  * Define the unique config of different poker styles.
+ * 
+ * TODO: Maybe this belongs in a GameStyle or GameFormat namespace.
+ * Which could also contain the blind format: No Limit / Pot Limit.
  */
 interface Game
 {
