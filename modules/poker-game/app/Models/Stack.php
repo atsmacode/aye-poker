3e4ae839c1d34237827728f9582ced2863536877
Mark <mark@Marks-MacBook-Pro.local>
2025-04-06 14:59:08 +0100

    Remove change method

--- modules/poker-game/app/Models/Stack.php
+++ modules/poker-game/app/Models/Stack.php
@@ -18,22 +18,4 @@ class Stack extends Model
     {
         return $this->amount;
     }
-
-    public function change(float $amount, int $playerId, int $tableId): ?int
-    {
-        try {
-            $queryBuilder = $this->connection->createQueryBuilder();
-            $queryBuilder
-                ->update('stacks')
-                ->set('amount', $queryBuilder->createNamedParameter($amount))
-                ->where('table_id = '.$queryBuilder->createNamedParameter($tableId))
-                ->andWhere('player_id = '.$queryBuilder->createNamedParameter($playerId));
-
-            return $queryBuilder->executeStatement();
-        } catch (\Exception $e) {
-            $this->logger->error($e->getMessage(), ['class' => self::class, 'method' => __METHOD__]);
-
-            return null;
-        }
-    }
 }
