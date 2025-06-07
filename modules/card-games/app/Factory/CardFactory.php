83aa5792028cb822375a13cad615b3839b65c429
Mark <mark@Marks-MacBook-Pro.local>
2025-06-07 09:11:20 +0100

    Add todo

--- modules/card-games/app/Factory/CardFactory.php
+++ modules/card-games/app/Factory/CardFactory.php
@@ -6,6 +6,8 @@ namespace Atsmacode\CardGames\Factory;
  * Factory class uing Card constants as a reference
  * to create test scenarios without calling models
  * and DB to generate Card objects/data.
+ * 
+ * TODO: use Enums instead
  */
 class CardFactory
 {
